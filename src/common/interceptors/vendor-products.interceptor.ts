10093989adb7f1f6a6203cdb6031bd31eb4b12a6
Eda Işıl İnal <edainalisil@gmail.com>
2025-12-12 08:26:14 +0300

    refactor(products): simplify vendor products interceptor to response normalization only

--- src/common/interceptors/vendor-products.interceptor.ts
+++ src/common/interceptors/vendor-products.interceptor.ts
@@ -1,43 +1,22 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
 import { Observable } from "rxjs";
-import { ResponseProductDto } from "src/products/dtos/response-product.dto";
 import { map } from 'rxjs/operators';
 import { sendResponse } from "src/helper/response.helper";
-import type { Product } from "generated/prisma";
-
 
 @Injectable()
 export class VendorProductsResponseInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        return next.handle().pipe(
-            map((data) => {
-                if (data === undefined || data === null) {
-                    return data;
-                }
-
-                // array (GET /vendor/products)
-                if (Array.isArray(data)) {
-                    const response = data.map((product) => this.toVendorProductsResponseDto(product));
-                    return sendResponse(response);
-                }
-
-                // (GET /vendor/products/:id, POST, PATCH)
-                const response = this.toVendorProductsResponseDto(data);
-                return sendResponse(response);
-            }),
-        );
+    intercept(
+      context: ExecutionContext,
+      next: CallHandler,
+    ): Observable<any> {
+      return next.handle().pipe(
+        map((data) => {
+          // delete 
+          if (data === undefined || data === null) {
+            return data;
+          }
+          return sendResponse(data);
+        }),
+      );
     }
-
-    private toVendorProductsResponseDto(product: Product): ResponseProductDto {
-        return {
-            id: product.id,
-            name: product.name,
-            description: product.description,
-            price: product.price,
-            stock: product.stock,
-            categoryId: product.categoryId,
-            createdAt: product.createdAt,
-            updatedAt: product.updatedAt,
-        };
-    }
-}
+  }
