075b6eafa2c895d276143a36db254895c910182d
Eda Işıl İnal <edainalisil@gmail.com>
2025-12-15 08:16:55 +0300

    refactor(stock-log): make stock log service transaction-aware

--- src/stock-log/stock-log.service.ts
+++ src/stock-log/stock-log.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { StockLogType } from "generated/prisma";
+import { Injectable } from '@nestjs/common';
+import { Prisma, StockLogType } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class StockLogService {
-    constructor(private readonly prisma: PrismaService) {}
-    async createStockLog(productId: string, quantity: number, type: StockLogType, variantId?: string) {
-        return this.prisma.stockLog.create({
+    constructor(private readonly prisma: PrismaService) { }
+
+    async createStockLog(
+        prismaClient: Prisma.TransactionClient | PrismaService,
+        params: {
+            productId: string;
+            quantity: number;
+            type: StockLogType;
+            variantId?: string;
+        },
+    ) {
+        return prismaClient.stockLog.create({
             data: {
-                productId,
-                quantity,
-                type,
-                variantId,
+                productId: params.productId,
+                quantity: params.quantity,
+                type: params.type,
+                variantId: params.variantId ?? null,
             },
         });
     }
