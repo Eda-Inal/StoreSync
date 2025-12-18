2aab7e8b1a32884e4c2bd0debd29b0bbdf8a2bd3
Eda Işıl İnal <edainalisil@gmail.com>
2025-12-18 09:15:49 +0300

    feat(order-pay): add POST /orders/:id/pay controller endpoint

--- src/orders/order.controller.ts
+++ src/orders/order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpCode, UseInterceptors } from "@nestjs/common";
+import { Controller, Post, Body, HttpCode, UseInterceptors, Param } from "@nestjs/common";
 import { OrdersService } from "./order.service";
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard";
 import { RolesGuard } from "src/auth/guards/roles.guard";
@@ -8,18 +8,24 @@ import { CreateOrdersDto } from "./dtos/create-orders.dto";
 import { User } from "src/common/decorators/user.decorator";
 import type { UserPayload } from "src/common/types/user-payload.type";
 import { OrdersResponseInterceptor } from "src/common/interceptors/order-response.interceptor";
+import { CreatePaymentDto } from "./dtos/pay/create-payment.dto";
 
 @Controller('orders')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles('USER')
-@UseInterceptors(OrdersResponseInterceptor)
 
 export class OrdersController {
     constructor(private readonly orderService: OrdersService) { }
 
     @Post()
     @HttpCode(201)
+    @UseInterceptors(OrdersResponseInterceptor)
     async create(@Body() createOrdersDto: CreateOrdersDto, @User() user: UserPayload) {
         return await this.orderService.create(createOrdersDto, user.id);
+    }
+
+    @Post(':id/pay')
+    async pay(@Body() createPaymentDto: CreatePaymentDto, @User() user: UserPayload, @Param('id') id: string) {
+        return await this.orderService.pay(createPaymentDto, user.id, id);
     }
 }
